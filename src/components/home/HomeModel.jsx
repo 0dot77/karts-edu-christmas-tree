afc4621334aa6ed261dcc6a94cedee0ca3f73706
0dot77 <yty0706@gmail.com>
2022-12-18 00:52:00 +0900

    조 이름 수정

--- src/components/home/HomeModel.jsx
+++ src/components/home/HomeModel.jsx
@@ -31,7 +31,7 @@ export default function HomeModel() {
           className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 w-max"
           onClick={() => nav('./team1')}
         >
-          캐롤
+          메리크리스마스
         </button>
         <button
           type="button"
@@ -45,7 +45,7 @@ export default function HomeModel() {
           className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 w-max"
           onClick={() => nav('./team3')}
         >
-          메리크리스마스
+          캐롤
         </button>
       </div>
       <Suspense
